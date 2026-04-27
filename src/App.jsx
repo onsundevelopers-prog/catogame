08302008f6b1f2e1b357c8ddffffc4a717c0cb7a
Ose <ose@example.com>
2026-04-27 19:23:56 -0400

    Add main.jsx and App.jsx to fix build

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <p>The home page is home.html</p>
+    </div>
+  );
+}
+
+export default App;
